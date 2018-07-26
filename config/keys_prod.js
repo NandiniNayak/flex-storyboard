c260236028dccb55ca61268ce87d18abc31bc1ae
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-07-26 16:09:58 +1000

    key update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -4,6 +4,6 @@ module.exports = {
 
   // connect to Atlas mongo: https://www.mongodb.com/cloud/atlas
   mongoURI: process.env.MONGO_URI,
-  googleClientID: process.env.GOGGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOGGLE_CLIENT_SECRET
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
